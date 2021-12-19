081dc04d6663f8e333a4e84498dd4d97fde6d313
Jobst Heitzig <heitzig@pik-potsdam.de>
2021-12-19 12:29:04 +0100

    extended example

--- src/test_target_function.py
+++ src/test_target_function.py
@@ -20,9 +20,27 @@ print("n_inputs:", n_inputs)
 x = 2 * np.random.rand(n_inputs)  # i.e., on average, nodes will do one test per year
 y = f.evaluate(x)
 
-print("One evaluation at random x:", y)
+print("\nOne evaluation at random x:", y)
+
+n_trials = 100
 
 # evaluate f a number of times at the same input:
-mean_y = np.mean([f.evaluate(x) for it in range(10)])
-print("Mean of ten evaluations at the same x:", mean_y)
+ys = np.array([f.evaluate(x) for it in range(n_trials)])
+print("Mean and std.dev. of", n_trials, "evaluations at the same random x:", ys.mean(), ys.std())
+print("Mean and std.dev. of the log of", n_trials, "evaluations at that x:", np.log(ys).mean(), np.log(ys).std())
+
+
+# do the same for an x that is based on the total number of incoming transmissions per node:
+
+target_list = f.model.transmissions_array[:, 3]
+values, counts = np.unique(target_list, return_counts=True)
+weights = np.zeros(n_inputs)
+weights[values] = counts
+shares = weights / weights.sum()
+total_budget = 1.0 * n_inputs
+
+x2 = shares * total_budget
+ys = np.array([f.evaluate(x2) for it in range(n_trials)])
+print("\nMean and std.dev. of", n_trials, "evaluations at a network-based x:", ys.mean(), ys.std())
+print("Mean and std.dev. of the log of", n_trials, "evaluations at that x:", np.log(ys).mean(), np.log(ys).std())
 
