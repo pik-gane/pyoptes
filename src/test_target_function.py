1aeba048203055ceabfa54b41ef750af9773c328
Jobst Heitzig <heitzig@pik-potsdam.de>
2021-12-19 12:38:23 +0100

    added histograms

--- src/test_target_function.py
+++ src/test_target_function.py
@@ -3,6 +3,7 @@ Simple test to illustrate how the target function could be used.
 """
 
 import numpy as np
+import pylab as plt
 from pyoptes import set_seed
 from pyoptes.optimization.budget_allocation import target_function as f
 
@@ -22,12 +23,13 @@ y = f.evaluate(x)
 
 print("\nOne evaluation at random x:", y)
 
-n_trials = 100
+n_trials = 1000
 
 # evaluate f a number of times at the same input:
 ys = np.array([f.evaluate(x) for it in range(n_trials)])
+logys = np.log(ys)
 print("Mean and std.dev. of", n_trials, "evaluations at the same random x:", ys.mean(), ys.std())
-print("Mean and std.dev. of the log of", n_trials, "evaluations at that x:", np.log(ys).mean(), np.log(ys).std())
+print("Mean and std.dev. of the log of", n_trials, "evaluations at that x:", logys.mean(), logys.std())
 
 
 # do the same for an x that is based on the total number of incoming transmissions per node:
@@ -40,7 +42,17 @@ shares = weights / weights.sum()
 total_budget = 1.0 * n_inputs
 
 x2 = shares * total_budget
-ys = np.array([f.evaluate(x2) for it in range(n_trials)])
-print("\nMean and std.dev. of", n_trials, "evaluations at a network-based x:", ys.mean(), ys.std())
-print("Mean and std.dev. of the log of", n_trials, "evaluations at that x:", np.log(ys).mean(), np.log(ys).std())
+ys2 = np.array([f.evaluate(x2) for it in range(n_trials)])
+logys2 = np.log(ys2)
+print("\nMean and std.dev. of", n_trials, "evaluations at a network-based x:", ys2.mean(), ys2.std())
+print("Mean and std.dev. of the log of", n_trials, "evaluations at that x:", logys2.mean(), logys2.std())
 
+plt.hist(ys)
+plt.hist(ys2)
+plt.title("histogram of f(x) for two different fixed values of x")
+plt.show()
+
+plt.hist(logys)
+plt.hist(logys2)
+plt.title("histogram of log(f(x)) for two different fixed values of x")
+plt.show()
