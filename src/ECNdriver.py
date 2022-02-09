419c254bfd731d0eabcb01099b7ad34957849d5b
JacobLoe <Jcbl423@gmail.com>
2022-02-09 17:54:34 +0100

    finished refactoring of ECNdriver

--- src/ECNdriver.py
+++ src/ECNdriver.py
@@ -30,37 +30,23 @@ def ECNoise(nf, fval):
     for j in range(nf-1):
         for i in range(nf-j-1):
             a = fval[i+1] - fval[i]
-            # print(a, fval[i+1], fval[i])
             fval[i] = a
 
-        # print(fval[0:nf-1].size - np.count_nonzero(fval[0:nf-1]), nf/2)
-        # print(fval[0:nf-1].sum())
-        # if j == 0 and fval[0:nf-1].size - np.count_nonzero(fval[0:nf-1]) >= nf/2:
-        #     inform = 2
-        #     return fnoise, level, inform
+        if j == 0 and fval[0:nf-1].size - np.count_nonzero(fval[0:nf-1]) >= nf/2:
+            inform = 2
+            return fnoise, level, inform
 
         jj = j+1
         gamma = 0.5*((jj)/(2*jj-1))*gamma
 
         # Compute the estimates for the noise level
         level[j] = np.sqrt(gamma*np.mean(fval[0:nf-j]**2))
-        # a = fval[0:nf-j]**2
-        # b = np.mean(a)
-        # c = gamma*b
-        # d = np.sqrt(c)
-        # print('\n',fval[0:nf-j])
-        # print(a)
-        # print(b)
-        # print(c)
-        # print(d)
 
         # determine differences in sign
         emin = fval[0:nf-j].min()
         emax = fval[0:nf-j].max()
         if emin*emax < 0.0:
             dsgn[j] = 0
-
-    print('dsgn', dsgn)
 
     for k in range(nf-3):
         emin = level[k:k+2].min()
@@ -72,16 +58,24 @@ def ECNoise(nf, fval):
 
     # If noise not detected then h is too large
     inform = 3
-    # print('post loop', fval)
     return fnoise, level, inform
 
 
-if __name__ == '__main__':
+def ECNdriver(n, m, h):
 
     # define the number of variables
     n = 10
-    # xb = np.random()
-    # p =
+    xb = np.random.rand(n)
+
+    p = np.random.rand(n)
+    for i in range(500):
+        if np.linalg.norm(p) > 1:
+            p = np.random.rand(n)
+        else:
+            print(p, p.sum(), np.linalg.norm(p))
+            break
+    p = p/np.linalg.norm(p)
+
     # Define the number of additional evaluations
     m = 8
 
@@ -93,18 +87,21 @@ if __name__ == '__main__':
 
     for i in range(0, m+1):
 
-        # s = 2*(i-mid)/m
-        # x = xb + s*h*p
-        fval[i] = 2.0042 #
+        s = 2*(i-mid)/m
+        x = xb + s*h*p
+        fval[i] = np.linalg.norm(x)#2.0042
 
-    print(fval)
-    print(mid)
+    fnoise, level, inform = ECNoise(9, fval)
 
-    # fnoise, level, inform = ECNoise(9, fval)
-    #
-    # print('fnoise', fnoise)
-    # print('level', level)
-    # print('inform', inform)
+    rel_noise = fnoise/mid
+
+    return fnoise, level, inform, rel_noise
 
 
+if __name__ == '__main__':
 
+    fnoise, level, inform, rel_noise = ECNdriver(n=10, m=8, h=1e-14)
+    print('fnoise', fnoise)
+    print('level', level)
+    print('inform', inform)
+    print('rel_noise', rel_noise)
