0b4e3eb2f9971372ed91af2c314845454b29bf2a
Malledive <m.roloff1@gmx.de>
2022-02-24 07:43:44 +0100

    commit

--- src/gnn.py
+++ src/gnn.py
@@ -31,57 +31,6 @@ from torch.nn import Sequential as Seq, Linear as Lin, ReLU
 from torch_scatter import scatter_mean
 from torch_geometric.nn import MetaLayer
 
-
-class EdgeModel(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.edge_mlp = Seq(Lin(2, 4), ReLU(), Lin(4, 2))
-
-    def forward(self, src, dest, edge_attr, u, batch):
-        # src, dest: [E, F_x], where E is the number of edges.
-        # edge_attr: [E, F_e]
-        # u: [B, F_u], where B is the number of graphs.
-        # batch: [E] with max entry B - 1.
-        print(src.shape, dest.shape, edge_attr.shape, u[batch].shape, batch.shape)
-
-        out = torch.cat([src, dest, edge_attr, u[batch]], 1)
-
-        return self.edge_mlp(out)
-
-class NodeModel(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.node_mlp_1 = Seq(Lin(..., ...), ReLU(), Lin(..., ...))
-        self.node_mlp_2 = Seq(Lin(..., ...), ReLU(), Lin(..., ...))
-
-    def forward(self, x, edge_index, edge_attr, u, batch):
-        # x: [N, F_x], where N is the number of nodes.
-        # edge_index: [2, E] with max entry N - 1.
-        # edge_attr: [E, F_e]
-        # u: [B, F_u]
-        # batch: [N] with max entry B - 1.
-        row, col = edge_index
-        out = torch.cat([x[row], edge_attr], dim=1)
-        out = self.node_mlp_1(out)
-        out = scatter_mean(out, col, dim=0, dim_size=x.size(0))
-        out = torch.cat([x, out, u[batch]], dim=1)
-        return self.node_mlp_2(out)
-
-class GlobalModel(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.global_mlp = Seq(Lin(..., ...), ReLU(), Lin(..., ...))
-
-    def forward(self, x, edge_index, edge_attr, u, batch):
-        # x: [N, F_x], where N is the number of nodes.
-        # edge_index: [2, E] with max entry N - 1.
-        # edge_attr: [E, F_e]
-        # u: [B, F_u]
-        # batch: [N] with max entry B - 1.
-        out = torch.cat([u, scatter_mean(x, batch, dim=0)], dim=1)
-        return self.global_mlp(out)
-
-
 def get_features(transmissions, capacities, G, time_covered):
     #print(time_covered)# 180
     edge_features = transmissions.groupby([2, 3]).size()
