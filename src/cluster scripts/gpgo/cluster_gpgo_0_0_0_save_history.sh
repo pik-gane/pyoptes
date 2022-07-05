f0c66c306ab03bd041b9ae1e90f3fa087db6437e
JacobLoe <jcbl423@gmail.com>
2022-07-05 12:51:01 +0200

    added missing experiments for cma

--- src/cluster scripts/gpgo/cluster_gpgo_0_0_0_save_history.sh
+++ src/cluster scripts/gpgo/cluster_gpgo_0_0_0_save_history.sh
@@ -25,7 +25,7 @@
 module load anaconda/5.0.0_py3
 source activate bbo
 srun -n $SLURM_NTASKS python3 /home/loebkens/pyoptes/src/bb_optimization.py gpgo 20220701_gpgo_rms_nodes_1040__no_prior_save_history \
-  --path_plot /home/loebkens/pyoptes/src/pyoptes/optimization/budget_allocation/blackbox_learning/plots/ \
+  --path_plot /home/loebkens/pyoptes_plots \
   --path_networks /p/projects/ou/labs/gane/optes/mcmc_100nets/data/ \
   --graph syn \
   --prior_mixed_strategies '' \
@@ -35,4 +35,3 @@ srun -n $SLURM_NTASKS python3 /home/loebkens/pyoptes/src/bb_optimization.py gpgo
   --scale_total_budget 1 \
   --save_test_strategies True \
   --use_prior '' \
-  --path_plot ../../pyoptes_plots
