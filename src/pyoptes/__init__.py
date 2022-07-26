f3b3d8802584a285abac26b4744da5dda01286e5
JacobLoe <Jcbl423@gmail.com>
2022-07-26 21:50:48 +0200

    removed unused smac and ax optimizer

--- src/pyoptes/__init__.py
+++ src/pyoptes/__init__.py
@@ -1,7 +1,5 @@
 from .util import *
 from .optimization.budget_allocation.blackbox_learning.bo_cma import *
-from .optimization.budget_allocation.blackbox_learning.bo_smac import *
-from .optimization.budget_allocation.blackbox_learning.bo_alebo import *
 from .optimization.budget_allocation.blackbox_learning.bo_pyGPGO import *
 from .optimization.budget_allocation.blackbox_learning.utils import *
 from .optimization.budget_allocation.blackbox_learning.utils_plots import *
