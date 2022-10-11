9a6532f374701cdfd2afc9dcd3a77f7cb083158a
JacobLoe <Jcbl423@gmail.com>
2022-10-11 19:28:53 +0200

    moved optimization results into data folder

--- src/bbo_inspect_prior.py
+++ src/bbo_inspect_prior.py
@@ -9,7 +9,7 @@ from pyoptes import compute_average_otf_and_stderr
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--path_plot', default='pyoptes/optimization/budget_allocation/blackbox_learning/plots/',
+    parser.add_argument('--path_plot', default='../data/blackbox_learning/results/',
                         help="Optimizer parameter. Location where all the individual results"
                              " of the optimizers are saved to. "
                              "Default location is 'pyoptes/optimization/budget_allocation/blackbox_learning/plots/'")
