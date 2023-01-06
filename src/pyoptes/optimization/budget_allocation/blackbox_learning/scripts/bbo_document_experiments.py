9954e5525743a237be6d8e02ed7fb29071960d74
JacobLoe <Jcbl423@gmail.com>
2023-01-06 15:02:30 +0100

    experiments table now includes experiment performance

--- src/pyoptes/optimization/budget_allocation/blackbox_learning/scripts/bbo_document_experiments.py
+++ src/pyoptes/optimization/budget_allocation/blackbox_learning/scripts/bbo_document_experiments.py
@@ -6,6 +6,8 @@ import os
 import glob
 import json
 import csv
+from tqdm import tqdm
+import numpy as np
 
 
 def bbo_document_experiments(path_plot):
@@ -34,38 +36,55 @@ def bbo_document_experiments(path_plot):
             param_set = param_set.union(optim_param_set)
             param_set = param_set.union(sim_param_set)
 
-        # TODO header needs to be extended with rows for optimizer performance metrics, e.g baseline, otf values, ratio
-        # write header
-        start_of_row = ['optimizer', 'n_nodes', 'sentinels', 'max_iterations', 'total_budget', 'statistic']
+        # write header, these are the hyperparameters that are shared by every experiment
+        # and will be the first columns to make the csv more readable
+        start_of_row = ['optimizer', 'n_nodes', 'sentinels', 'max_iterations', 'total_budget', 'statistic',
+                        'ratio', 'otf', 'baseline', 'time (in hours)']
         # remove start_of_row from param_set
         param_set = param_set.difference(set(start_of_row))
         param_list = list(param_set)
 
-        # extend start_of_row with param_list to create new param_list
+        # extend start_of_row with param_list to create new param_list with all parameters in the desired order
         param_list = start_of_row + sorted(param_list)
 
         cswriter.writerow(param_list)
 
         # write entries for each experiment
-        for experiment_params in path_to_experiments:
+        for experiment_params in tqdm(path_to_experiments):
 
+            path_to_eval_output = os.path.join(os.path.dirname(experiment_params), 'evaluation_output.txt')
+            if os.path.isfile(path_to_eval_output):
 
-            # path_to_eval_output = os.path.join(os.path.dirname(experiment_params), 'evaluation_output.txt')
+                # read the optimization results from the text-file
+                with open(path_to_eval_output, 'r') as f:
+                    lines = f.readlines()
+                ratio_otf_baseline = np.round(float(lines[1].split(':')[-1]), decimals=2)
+                baseline = np.round(float(lines[2].split(':')[-1].split(',')[0]), decimals=2)
+                otf = np.round(float(lines[3].split(':')[-1].split(',')[0]), decimals=2)
+                time_to_optimize = np.round(float(lines[4].split(':')[-1]), decimals=2)
+                evaluation_results = {'ratio': ratio_otf_baseline,
+                                      'baseline': baseline,
+                                      'otf': otf,
+                                      'time (in hours)': time_to_optimize}
 
-            # TODO add check for 'evaluation_output.txt' file, if not present, skip experiment
-            with open(experiment_params, 'r') as f:
-                hyperparameters = json.load(f)
+                # read the hyperparameter for the .json-file
+                with open(experiment_params, 'r') as f:
+                    hyperparameters = json.load(f)
 
-            optimizer_hyperparameters = hyperparameters['optimizer_hyperparameters']
-            simulation_hyperparameters = hyperparameters['simulation_hyperparameters']
+                optimizer_hyperparameters = hyperparameters['optimizer_hyperparameters']
+                simulation_hyperparameters = hyperparameters['simulation_hyperparameters']
 
-            row = []
-            for param in param_list:
-                if param in optimizer_hyperparameters:
-                    row.append(optimizer_hyperparameters[param])
-                elif param in simulation_hyperparameters:
-                    row.append(simulation_hyperparameters[param])
-                else:
-                    row.append('')
+                row = []
+                for param in param_list:
+                    if param in optimizer_hyperparameters:
+                        row.append(optimizer_hyperparameters[param])
+                    elif param in simulation_hyperparameters:
+                        row.append(simulation_hyperparameters[param])
+                    elif param in evaluation_results:
+                        row.append(evaluation_results[param])
+                    else:
+                        row.append('')
 
-            cswriter.writerow(row)
+                cswriter.writerow(row)
+            else:
+                pass
