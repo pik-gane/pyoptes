815b66409c863bfcf5ebe99ff1f8aa5709a5d4c7
JacobLoe <Jcbl423@gmail.com>
2023-01-16 01:07:34 +0100

    cma and np experiments

--- src/cluster scripts/cma/cluster_cma_0_1_0_1000_iterations.sh
+++ src/cluster scripts/cma/cluster_cma_0_1_0_1000_iterations.sh
@@ -4,13 +4,13 @@
 
 #SBATCH --qos=medium
 
-#SBATCH --job-name=loebkens_np_2_1_0_750_iterations
+#SBATCH --job-name=loebkens_cma_0_1_0_1000_iterations
 
 #SBATCH --account=gane
 
-#SBATCH --output=logs/outputs_np_2_1_0_750_iterations.out
+#SBATCH --output=logs/outputs_cma_0_1_0_1000_iterations.out
 
-#SBATCH --error=logs/errors_np_2_1_0_750_iterations.err
+#SBATCH --error=logs/errors_cma_0_1_0_1000_iterations.err
 
 #SBATCH --workdir=/home/loebkens
 
@@ -25,22 +25,17 @@
 module load anaconda/5.0.0_py3
 source activate bbo
 srun -n $SLURM_NTASKS python3 /home/loebkens/pyoptes/src/black-box-optimization.py optimization \
-  --optimizer np \
-  --name_experiment 20230109_np_nodes_120_750_iterations \
+  --optimizer cma \
+  --name_experiment 20230109_cma_mean_nodes_1040_1000_iterations \
   --path_plot /home/loebkens/pyoptes/data/blackbox_learning/results/ \
   --path_networks /home/loebkens/network/data \
   --graph syn \
-  --prior_mixed_strategies '' \
-  --n_nodes 120 \
-  --sentinels 120 \
+  --n_nodes 1040 \
+  --sentinels 1040 \
   --statistic mean \
   --scale_total_budget 1 \
-  --r_dim 50 \
-  --z_dim 50 \
-  --h_dim 50 \
-  --num_target 3 \
-  --num_context 3 \
-  --batch_size 10 \
-  --epochs 30 \
-  --max_iterations 750 \
-  --n_runs 1
+  --prior_mixed_strategies '' \
+  --popsize 9 \
+  --max_iterations 1000 \
+  --n_runs 1 \
+
--- src/cluster scripts/neural_process/cluster_np_2_1_0_400_iterations.sh
+++ src/cluster scripts/neural_process/cluster_np_2_1_0_400_iterations.sh
@@ -24,9 +24,11 @@
 
 module load anaconda/5.0.0_py3
 source activate bbo
-srun -n $SLURM_NTASKS python3 /home/loebkens/pyoptes/src/bb_optimization.py np 20221021_np_mean_nodes_120_400_iterations \
+srun -n $SLURM_NTASKS python3 /home/loebkens/pyoptes/src/black-box-optimization.py optimization \
+  --optimizer np \
+  --name_experiment 20230109_np_nodes_120_400_iterations \
   --path_plot /home/loebkens/pyoptes/data/blackbox_learning/results/ \
-  --path_networks /p/projects/ou/labs/gane/optes/mcmc_100nets/data/ \
+  --path_networks /home/loebkens/network/data \
   --graph syn \
   --prior_mixed_strategies '' \
   --n_nodes 120 \
@@ -41,4 +43,4 @@ srun -n $SLURM_NTASKS python3 /home/loebkens/pyoptes/src/bb_optimization.py np 2
   --batch_size 10 \
   --epochs 30 \
   --max_iterations 400 \
-  --n_runs 100
+  --n_runs 1
