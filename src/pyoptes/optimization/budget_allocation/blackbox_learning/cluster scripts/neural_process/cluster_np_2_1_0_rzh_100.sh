724c4e3246a18b24adc8608823218e2ea73a248a
JacobLoe <Jcbl423@gmail.com>
2023-01-20 12:48:51 +0100

    np cluster scripts for 120 nodes and new architecture

---
+++ src/pyoptes/optimization/budget_allocation/blackbox_learning/cluster scripts/neural_process/cluster_np_2_1_0_rzh_100.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#SBATCH --constraint=broadwell
+
+#SBATCH --qos=medium
+
+#SBATCH --job-name=loebkens_np_2_1_0_rzh_100
+
+#SBATCH --account=gane
+
+#SBATCH --output=logs/outputs_np_2_1_0_rzh_100.out
+
+#SBATCH --error=logs/errors_np_2_1_0_rzh_100.err
+
+#SBATCH --workdir=/home/loebkens
+
+#SBATCH --nodes=1      # nodes requested
+
+#SBATCH --ntasks=1      # tasks requested
+
+#SBATCH --cpus-per-task=32
+
+#SBATCH --mem=64000
+
+module load anaconda/5.0.0_py3
+source activate bbo
+srun -n $SLURM_NTASKS python3 /home/loebkens/pyoptes/src/black-box-optimization.py optimization \
+  --optimizer np \
+  --name_experiment 20230120_np_mean_nodes_120_rzh_100 \
+  --path_plot /home/loebkens/pyoptes/data/blackbox_learning/results/ \
+  --path_networks /home/loebkens/network/data \
+  --graph syn \
+  --prior_mixed_strategies '' \
+  --n_nodes 120 \
+  --sentinels 120 \
+  --statistic mean \
+  --scale_total_budget 1 \
+  --r_dim 100 \
+  --z_dim 100 \
+  --h_dim 100 \
+  --num_target 3 \
+  --num_context 3 \
+  --batch_size 10 \
+  --epochs 30 \
+  --max_iterations 30 \
+  --n_runs 100
