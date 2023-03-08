1a892773d8289b54e144482f735c496acbca0d4b
JacobLoe <Jcbl423@gmail.com>
2023-03-08 02:26:14 +0100

    scripts

--- src/pyoptes/optimization/budget_allocation/blackbox_learning/cluster scripts/neural_process/cluster_np_0_1_1_50_iterations.sh
+++ src/pyoptes/optimization/budget_allocation/blackbox_learning/cluster scripts/neural_process/cluster_np_0_1_1_50_iterations.sh
@@ -4,13 +4,13 @@
 
 #SBATCH --qos=short
 
-#SBATCH --job-name=loebkens_np_1_1_0_4N_budget
+#SBATCH --job-name=loebkens_np_0_1_1_50_iterations
 
 #SBATCH --account=gane
 
-#SBATCH --output=logs/outputs_np_1_1_0_4N_budget.out
+#SBATCH --output=logs/outputs_np_0_1_1_50_iterations.out
 
-#SBATCH --error=logs/errors_np_1_1_0_4N_budget.err
+#SBATCH --error=logs/errors_np_0_1_1_50_iterations.err
 
 #SBATCH --workdir=/home/loebkens
 
@@ -26,13 +26,13 @@ module load anaconda/5.0.0_py3
 source activate bbo
 srun -n $SLURM_NTASKS python3 /home/loebkens/pyoptes/src/black-box-optimization.py optimization \
   --optimizer np \
-  --name_experiment 20230301_np_mean_nodes_57590_4N_budget \
+  --name_experiment 20230308_np_mean_nodes_1040_4N_budget_50_iterations \
   --path_plot /home/loebkens/pyoptes/data/blackbox_learning/results/ \
   --path_networks /home/loebkens/network/data \
   --graph syn \
   --prior_mixed_strategies '' \
-  --n_nodes 57590 \
-  --sentinels 1329 \
+  --n_nodes 1040 \
+  --sentinels 1040 \
   --statistic mean \
   --scale_total_budget 4 \
   --r_dim 50 \
@@ -42,5 +42,5 @@ srun -n $SLURM_NTASKS python3 /home/loebkens/pyoptes/src/black-box-optimization.
   --num_context 3 \
   --batch_size 10 \
   --epochs 30 \
-  --max_iterations 30 \
-  --n_runs 10
+  --max_iterations 50 \
+  --n_runs 100
---
+++ src/pyoptes/optimization/budget_allocation/blackbox_learning/cluster scripts/neural_process/cluster_np_1_1_2_50_iterations.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#SBATCH --constraint=broadwell
+
+#SBATCH --qos=short
+
+#SBATCH --job-name=loebkens_np_1_1_2_50_iterations
+
+#SBATCH --account=gane
+
+#SBATCH --output=logs/outputs_np_1_1_2_50_iterations.out
+
+#SBATCH --error=logs/errors_np_1_1_2_50_iterations.err
+
+#SBATCH --workdir=/home/loebkens
+
+#SBATCH --nodes=1      # nodes requested
+
+#SBATCH --ntasks=1      # tasks requested
+
+#SBATCH --cpus-per-task=32
+
+#SBATCH --mem=64000
+
+module load anaconda/5.0.0_py3
+source activate bbo
+srun -n $SLURM_NTASKS python3 /home/loebkens/pyoptes/src/black-box-optimization.py optimization \
+  --optimizer np \
+  --name_experiment 20230308_np_mean_nodes_57590_12N_budget_50_iterations \
+  --path_plot /home/loebkens/pyoptes/data/blackbox_learning/results/ \
+  --path_networks /home/loebkens/network/data \
+  --graph syn \
+  --prior_mixed_strategies '' \
+  --n_nodes 57590 \
+  --sentinels 1329 \
+  --statistic mean \
+  --scale_total_budget 12 \
+  --r_dim 50 \
+  --z_dim 50 \
+  --h_dim 50 \
+  --num_target 3 \
+  --num_context 3 \
+  --batch_size 10 \
+  --epochs 30 \
+  --max_iterations 50 \
+  --n_runs 10
